62bdc838279661e2a059782b458ce6af021c564d
david-0508 <david-0508@gmail.com>
2024-08-17 14:43:00 +0800

    modify

--- 2024_08_17/buttom1.py
+++ 2024_08_17/buttom1.py
@@ -1,3 +1,4 @@
+
 import signal
 from gpiozero import Button,LED
 from datetime import datetime
@@ -7,20 +8,29 @@ def user_release():
     print("使用者按下放開")
     led.toggle()
     now = datetime.now()
-    now_str = now.strftime('%Y-%m-%d %H:%M:%S:')
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     print(now_str)
     if led.is_lit:
-        message = "燈是開的"
+        message = '''{
+            "status":true,
+            "date":"2024/08/18 14:22:56",
+            "topic":"501教室/桌燈"
+        }'''
         print(message)
         publish.single(topic='501教室/桌燈',payload=message,hostname='127.0.0.1',qos=2)
     else:
-        message = "燈是關的"
+        message = '''{
+            "status":false,
+            "date":"2024/08/18 14:22:56",
+            "topic":"501教室/桌燈"
+        }'''
         print(message)
         publish.single(topic='501教室/桌燈',payload=message,hostname='127.0.0.1',qos=2)
 
 if __name__ == '__main__':
     button = Button(pin=18)
     button.when_released = user_release
-    led = LED(pin=25)
+    led = LED(pin=25) 
     signal.pause()
+\
 
